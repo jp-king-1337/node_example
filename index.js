f757856e922eb470f16b7484b67ba884e5cbbfb4
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-28 13:35:21 -0400

    Leaning

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const app = express();
 
-app.get()
+app.get("/", (clientRequestObject, serverResponseObject) => {
+    serverResponseObject.send("Hello there!");
+});
+
+app.listen(3333);
