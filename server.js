b51a479a745a6c016d557099d7560e6c75098e72
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-29 12:10:47 -0400

    Changed to double quotes, changed from JD to JP

--- server.js
+++ server.js
@@ -1,48 +1,48 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 console.log(__dirname);
 
 const people = [
-    {
-        id: 1,
-        name: 'JD',
-        age: 43
-    },
-    {
-        id: 2,
-        name: 'Bob',
-        age: 99
-    }
+  {
+    id: 1,
+    name: "JP",
+    age: 26
+  },
+  {
+    id: 2,
+    name: "Bob",
+    age: 99
+  }
 ];
 
 
 // Will send the index.html file as response
-app.get('/', (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (clientRequestObject, serverResponseObject) => {
+  serverResponseObject.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/api/people', (clientRequestObject, serverResponseObject) => {
+app.get("/api/people", (clientRequestObject, serverResponseObject) => {
 
-    if (!clientRequestObject.query.id) return serverResponseObject.send(people);
+  if (!clientRequestObject.query.id) return serverResponseObject.send(people);
 
-    const userObj = people.find(personObj => personObj.id == clientRequestObject.query.id);
+  const userObj = people.find(personObj => personObj.id == clientRequestObject.query.id);
 
-    serverResponseObject.send(userObj ? userObj : {
-        message: 'User not found',
-        error: 404
-    });
+  serverResponseObject.send(userObj ? userObj : {
+    message: "User not found",
+    error: 404
+  });
 });
 
-app.get('/api/person/:id', (clientRequestObject, serverResponseObject) => {
-    const userObj = people.find(personObj => personObj.id == clientRequestObject.params.id);
+app.get("/api/person/:id", (clientRequestObject, serverResponseObject) => {
+  const userObj = people.find(personObj => personObj.id == clientRequestObject.params.id);
 
-    serverResponseObject.send(userObj ? userObj : {
-        message: 'User not found',
-        error: 404
-    });
+  serverResponseObject.send(userObj ? userObj : {
+    message: "User not found",
+    error: 404
+  });
 });
 
-app.listen(3333, () => console.log('Server started on port 3333.'));
+app.listen(3333, () => console.log("Server started on port 3333."));
