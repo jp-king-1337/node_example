d10da8626d7bca1189822a0144a587d58d553c92
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-29 12:09:43 -0400

    Copied in JD's code to make sure I have exactly what he has

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
@@ -8,53 +8,41 @@ console.log(__dirname);
 const people = [
     {
         id: 1,
-        name: "JP",
-        age: 26
+        name: 'JD',
+        age: 43
     },
     {
         id: 2,
-        name: "Bob",
+        name: 'Bob',
         age: 99
     }
 ];
 
 
 // Will send the index.html file as response
-app.get("/", (clientRequestObject, serverResponseObject) => {
-    serverResponseObject.sendFile(path.join(__dirname, "index.html"));
+app.get('/', (clientRequestObject, serverResponseObject) => {
+    serverResponseObject.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get("/api/people", (clientRequestObject, serverResponseObject) => {
+app.get('/api/people', (clientRequestObject, serverResponseObject) => {
 
-    // We're using the return below to avoid the else in this conditional.
     if (!clientRequestObject.query.id) return serverResponseObject.send(people);
 
-    const userObj = people.find((personObj) => personObj.id == clientRequestObject.query.id);
+    const userObj = people.find(personObj => personObj.id == clientRequestObject.query.id);
 
-    // Ternary expression below! 
     serverResponseObject.send(userObj ? userObj : {
-        message: "User not found.",
+        message: 'User not found',
         error: 404
     });
 });
 
-app.get("/api/person/:id", (clientRequestObject, serverResponseObject) => {
+app.get('/api/person/:id', (clientRequestObject, serverResponseObject) => {
     const userObj = people.find(personObj => personObj.id == clientRequestObject.params.id);
 
     serverResponseObject.send(userObj ? userObj : {
-        message: "User not found.",
+        message: 'User not found',
         error: 404
     });
 });
 
-app.listen(3333, () => console.log("Server started on port 3333."));
-
-
-
-
-
-
-// Query Param Activity
-// Visit this url - http://localhost:3333/api/people?id=1
-// Grab the id from the query param list and use it to send the user object matching the id
-// If the id does not match a user, then send back a “cannot find user” message.\
+app.listen(3333, () => console.log('Server started on port 3333.'));
