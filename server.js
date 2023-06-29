de447accb164d562cfc3587d851314c2338fece4
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-29 11:24:01 -0400

    Query Param activity completed with JD in class.

--- server.js
+++ server.js
@@ -1,35 +1,48 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 console.log(__dirname);
 
 const people = [
-  {
-    id: 1,
-    name: 'JD',
-    age: 43
-  },
-  {
-    id: 2,
-    name: 'Bob',
-    age: 99
-  }
+    {
+        id: 1,
+        name: "JP",
+        age: 26
+    },
+    {
+        id: 2,
+        name: "Bob",
+        age: 99
+    }
 ];
 
 
 // Will send the index.html file as response
-app.get('/', (clientRequestObject, serverResponseObject) => {
-  serverResponseObject.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (clientRequestObject, serverResponseObject) => {
+    serverResponseObject.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('/api/people', (clientRequestObject, serverResponseObject) => {
-  console.log(clientRequestObject.query);
-  serverResponseObject.send({
-    message: 'Wow, we made an API',
-    date: new Date()
-  })
+app.get("/api/people", (clientRequestObject, serverResponseObject) => {
+    const userObj = people.find((personObj) => {
+        return personObj.id == clientRequestObject.query.id
+    });
+
+    serverResponseObject.send(userObj ? userObj : {
+        message: "User not found.",
+        error: 404
+    });
 });
 
-app.listen(3333, () => console.log('Server started on port 3333.'));
+app.listen(3333, () => console.log("Server started on port 3333."));
+
+
+
+
+
+
+// Query Param Activity
+// Visit this url - http://localhost:3333/api/people?id=1
+// Grab the id from the query param list and use it to send the user object matching the id
+// If the id does not match a user, then send back a “cannot find user” message.\
