b445b6df843296e0bbf97de16de1468e07df1d25
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-06-30 11:16:17 -0400

    save point again

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(express.static('./public'));
 // Load Routes
 
 // Post route to retrieve the form data
-app.post("/notes", );
+app.post("/notes", (clientReq, serverRes) => {
+    
+});
 
 app.listen(3333, () => console.log('Server started on port 3333.'));
