552cc6df21e667643ac16fda7790c1ac959ced41
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-07 14:19:50 -0400

    Updated to JD's code to follow along in class

--- models/note.js
+++ models/note.js
@@ -1,30 +1,30 @@
-const fs = require("fs");
-const { v4 } = require("uuid");
-const path = require("path");
-const DB_PATH = path.join(__dirname, "../db/notes.json");
+const fs = require('fs');
+const { v4 } = require('uuid');
+const path = require('path');
+const DB_PATH = path.join(__dirname, '../db/notes.json');
 
 function getNotes() {
-    const notes = JSON.parse(fs.readFileSync(DB_PATH, "utf8"));
-    return notes;
+  const notes = JSON.parse(fs.readFileSync(DB_PATH, 'utf8'));
+  return notes;
 }
 
 class Note {
-    constructor(text) {
-        this.id = v4();
-        this.text = text;
-    }
+  constructor(text) {
+    this.id = v4();
+    this.text = text;
+  }
 
-    save() {
-        const notes = getNotes();
+  save() {
+    const notes = getNotes();
 
-        notes.push(this);
+    notes.push(this);
 
-        fs.writeFile(DB_PATH, JSON.stringify(notes, null, 2), err => {
-            if (err) throw err;
+    fs.writeFile(DB_PATH, JSON.stringify(notes, null, 2), err => {
+      if (err) throw err;
 
-            console.log("Note saved successfully!");
-        });
-    }
+      console.log('Note saved successfully!');
+    })
+  }
 }
 
 module.exports = { Note: Note, getNotes: getNotes };
--- server.js
+++ server.js
@@ -1,30 +1,15 @@
-const express = require("express");
-const { Note, getNotes } = require("./models/Note");
-// const path = require("path");
+const express = require('express');
+const view_routes = require('./routes/view_routes');
+const api_routes = require('./routes/api_routes');
+const PORT = process.env.PORT || 3333;
+// const path = require('path');
 
 const app = express();
 
-app.use(express.static("./public"));
+app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: false }));
 
 // Load Routes
+app.use('/', [view_routes, api_routes]);
 
-// Post route to retrieve the form data
-app.post("/notes", (clientReq, serverRes) => {
-
-    const newNote = new Note(clientReq.body.note);
-
-    newNote.save();
-
-    // Respond back to the client to complete the request
-    serverRes.redirect("/");
-});
-
-app.get("/notes", (clientReq, serverRes) => {
-    const notes = getNotes();
-
-    // .send will stringify to JSON for you
-    serverRes.send(notes);
-})
-
-app.listen(3333, () => console.log("Server started on port 3333."));
+app.listen(PORT, () => console.log('Server started on port 3333.'));
